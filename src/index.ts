a587d85c622e16f9b79f32c39ff63f67c2a43322
Daniel <imoleczek@gmail.com>
2023-08-11 00:12:16 +0200

    connection to database

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { connect } from 'mongoose';
 import { init, loadEvents, loadCommand } from './base/Client';
 import { config } from './config';
 
@@ -11,3 +12,5 @@ export const commands = []
 
 loadEvents(bot, './src/events')
 loadCommand(bot, './src/commands')
+
+connect('mongodb://127.0.0.1:27017/FlashBot')
