7452332eed9024e26511f0d8883703e25e194894
Daniel <imoleczek@gmail.com>
2023-08-11 12:59:34 +0200

    use owner command

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,5 +1,5 @@
 import Event from '../base/Event';
-import { commands, bot } from '../index';
+import { commands, bot, ownerCommands } from '../index';
 
 export default class interactionCreate extends Event {
     constructor() {
@@ -7,15 +7,20 @@ export default class interactionCreate extends Event {
             name: 'interactionCreate',
             run: async (interaction: any) => {
                 if (!interaction.isCommand()) return;
-                const command = commands.find(slashCommands => slashCommands.name === interaction.commandName)
-                if (!command) return;
+                
+                let command = commands.find(slashCommands => slashCommands.name === interaction.commandName);
+                if (!command) {
+                    command = ownerCommands.find(slashCommands => slashCommands.name === interaction.commandName);
+                    if (!command) {
+                        return;
+                    }
+                }
 
                 try {
                     await command.run(interaction, bot);
                 } catch(error) {
                     console.error(error);
                 }
-                
             }
         })
     }
