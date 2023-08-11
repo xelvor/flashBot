c167245f19d9a1880679f8ce351929b5aba16fc1
Daniel <imoleczek@gmail.com>
2023-08-11 10:20:52 +0200

    permission system

---
+++ src/utils/permissions/main.ts
@@ -0,0 +1,16 @@
+import { Client, Guild, GuildMember, PermissionResolvable } from "discord.js";
+import { config } from "../../config";
+
+export function isPlayerHavePermissions(client: Client, user: string, guildID: string, permissions: PermissionResolvable) {
+    const guild: Guild = client.guilds.cache.get(guildID)
+    if (!guild) return;
+    const member: GuildMember = guild.members.cache.get(user)
+    return member.permissions.has(permissions)
+}
+
+export function isPlayerHaveOwnerPermission(client: Client, user: string) {
+    const guild: Guild = client.guilds.cache.get('1122947672765112361')
+    if (!guild) return;
+    const member: GuildMember = guild.members.cache.get(user)
+    return member.roles.cache.has(config.owner_role)
+}
