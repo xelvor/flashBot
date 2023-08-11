5cb16c486c46a66bf1cfe266aca86ee2be2e59c7
Daniel <imoleczek@gmail.com>
2023-08-11 08:57:09 +0200

    custom badges

---
+++ src/utils/badges/main.ts
@@ -0,0 +1,10 @@
+export const badges: object = [
+    {
+        name: 'Bot owner',
+        icon: '<:owner:1139452297450098820>'
+    },
+    {
+        name: 'Bot staff',
+        icon: '<:staff:1139452295038378016>'
+    }
+]
