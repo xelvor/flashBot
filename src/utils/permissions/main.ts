8986121b5617006dbd2e28dd612c412fca9f90c8
Daniel <imoleczek@gmail.com>
2023-08-11 13:16:18 +0200

    fixed permissions

--- src/utils/permissions/main.ts
+++ src/utils/permissions/main.ts
@@ -1,16 +1,15 @@
 import { Client, Guild, GuildMember, PermissionResolvable } from "discord.js";
 import { config } from "../../config";
 
-export function isPlayerHavePermissions(client: Client, user: string, guildID: string, permissions: PermissionResolvable) {
-    const guild: Guild = client.guilds.cache.get(guildID)
+export async function isPlayerHavePermissions(client: Client, user: string, guildID: string, permissions: PermissionResolvable) {
+    const guild: Guild = (await client).guilds.cache.get(guildID)
     if (!guild) return;
     const member: GuildMember = guild.members.cache.get(user)
     return member.permissions.has(permissions)
 }
 
-export function isPlayerHaveOwnerPermission(client: Client, user: string) {
-    console.log(client.guilds)
-    const guild: Guild = client.guilds.cache.get('1122947672765112361')
+export async function isPlayerHaveOwnerPermission(client: Client, user: string) {
+    const guild: Guild = (await client).guilds.cache.get('1122947672765112361')
     if (!guild) return;
     const member: GuildMember = guild.members.cache.get(user)
     return member.roles.cache.has(config.owner_role)
