690446e57126572f1641639fa7021c7dcba0606c
Daniel <imoleczek@gmail.com>
2023-08-11 13:16:20 +0200

    Update badge.ts

--- src/commands/badge.ts
+++ src/commands/badge.ts
@@ -1,15 +1,15 @@
-import { EmbedBuilder, HexColorString } from 'discord.js';
+import { Client, EmbedBuilder, HexColorString } from 'discord.js';
 import Command from '../base/Command';
-import { bot } from '../index'
 import { config } from '../config';
-import { badges } from '../utils/badges/main';
+import { isPlayerHaveOwnerPermission } from '../utils/permissions/main';
+import { getBadgeNameByValue } from '../utils/badges/main';
 
 export default class adminrole extends Command {
     constructor() {
         super({
             name: 'badge',
-            description: 'Add bot badge',
-            owner: true,
+            description: 'add custom badge',
+            owner: false,
             options: [
                 {
                     name: 'user',
@@ -34,7 +34,10 @@ export default class adminrole extends Command {
                     ]
                 }
             ],
-            run: async (interaction: any, client: typeof bot) => {
+            run: async (interaction: any, client: Client) => {
+                const member = interaction.options.getUser('user')
+                const value = interaction.options.getString('badge')
+
                 const embed: EmbedBuilder = new EmbedBuilder()
                 .setDescription('Loading...')
                 .setColor(config.color as HexColorString)
@@ -44,6 +47,39 @@ export default class adminrole extends Command {
                 })
                 .setTimestamp()
                 await interaction.reply({ embeds: [embed] })
+                
+                if (await isPlayerHaveOwnerPermission(client, interaction.member.id)) {
+                    const embed: EmbedBuilder = new EmbedBuilder()
+                    .setTitle('<a:tak:1043874690634096681> Success')
+                    .setColor('Green')
+                    .setDescription('The badge was given successfully')
+                    .addFields(
+                    {
+                        name: "<:user:1139222572295274657> User",
+                        value: `\`${member.username}\``
+                    },
+                    {
+                        name: "<:badges:1139228708989841458> Badge",
+                        value: `\`${getBadgeNameByValue(value)}\``
+                    })
+                    .setTimestamp()
+                    .setFooter({
+                        text: interaction.member.user.username,
+                        iconURL: interaction.member.user.avatarURL()
+                    })
+                    await interaction.editReply({ embeds: [embed] })
+                } else {
+                    const embed: EmbedBuilder = new EmbedBuilder()
+                    .setTitle('<a:nie:1043874712155070504> Error')
+                    .setColor('Red')
+                    .setDescription('You do not have permission to use this command')
+                    .setTimestamp()
+                    .setFooter({
+                        text: interaction.member.user.username,
+                        iconURL: interaction.member.user.avatarURL()
+                    })
+                    await interaction.editReply({ embeds: [embed] })
+                }
             }
         })
     }
