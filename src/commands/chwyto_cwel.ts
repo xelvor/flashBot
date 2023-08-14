85993a2b8cc3511feacdc4594fb6523acc928cf4
Daniel <imoleczek@gmail.com>
2023-08-14 19:36:00 +0200

    Update chwyto_cwel.ts

--- src/commands/chwyto_cwel.ts
+++ src/commands/chwyto_cwel.ts
@@ -1,7 +1,10 @@
-import { EmbedBuilder, HexColorString, AttachmentBuilder } from 'discord.js';
 import Command from '../base/Command';
-import { bot } from '../index'
-import { config } from '../config';
+import { bot } from '../index';
+import { createCanvas, loadImage, registerFont } from 'canvas';
+
+const font = './src/utils/fonts/Inter-SemiBold.ttf'
+
+registerFont(font, { family: 'Inter' });
 
 export default class adminrole extends Command {
     constructor() {
@@ -30,18 +33,60 @@ export default class adminrole extends Command {
                 }
             ],
             run: async (interaction: any, client: typeof bot) => {
-                const embed: EmbedBuilder = new EmbedBuilder()
-                .setDescription('Loading...')
-                .setColor(config.color as HexColorString)
-                .setFooter({
-                    text: interaction.member.user.username,
-                    iconURL: interaction.member.user.avatarURL()
-                })
-                .setTimestamp()
-                await interaction.reply({ embeds: [embed] })
+                let text: string = interaction.options.getString('text');
+                const channel = interaction.options.getChannel('channel');
+                const attachment = interaction.options.getAttachment('attachment');
+            
+                let backgroundImage = await loadImage('https://cdn.discordapp.com/attachments/1123184850107637831/1140647084207394928/bg.png');
+            
+                const canvas = createCanvas(1100, 500);
+                const ctx = canvas.getContext('2d');
+            
+                if (backgroundImage) {
+                    ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
+                } else {
+                    ctx.fillStyle = '#FFFFFF';
+                    ctx.fillRect(0, 0, canvas.width, canvas.height);
+                }
 
+                text = text.replace("{username}", `${interaction.member.user.username}`)
+            
+                ctx.fillStyle = '#E6E6E6';
+                ctx.font = '32px Inter';
+                ctx.textAlign = 'center';
+                ctx.fillText(text, 550, 400);
+            
+                const avatarSize = 250;
+                const avatarX = 425;
+                const avatarY = 95;
+                const avatarRadius = avatarSize / 2;
+
+                const userAvatar = await loadImage(interaction.member.user.displayAvatarURL({ format: 'png' }));
                 
+                ctx.beginPath();
+                ctx.ellipse(avatarX + avatarRadius, avatarY + avatarRadius, avatarRadius + 5, avatarRadius + 5, 0, 0, Math.PI * 2);
+                ctx.closePath();
+                ctx.clip();
+                ctx.drawImage(userAvatar, avatarX - 5, avatarY - 5, avatarSize + 10, avatarSize + 10);
+                ctx.beginPath();
+                ctx.arc(avatarX + avatarSize / 2, avatarY + avatarSize / 2, avatarSize / 2, 0, Math.PI * 2);
+                ctx.lineWidth = 9; 
+                ctx.strokeStyle = '#E6E6E6';
+                ctx.stroke();
+                ctx.closePath();
+                ctx.restore();
+            
+                const attachmentBuffer = canvas.toBuffer();
+            
+                await channel.send({
+                    content: `asdd`,
+                    files: [{
+                        attachment: attachmentBuffer,
+                        name: 'flashbot-welcome.png'
+                    }]
+                });
+            
             }
-        })
+        });
     }
 }
