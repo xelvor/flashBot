508d9196c8e7a048648884ad35f2fece71deaa36
Daniel <imoleczek@gmail.com>
2023-08-14 19:36:15 +0200

    Create guild.ts

---
+++ src/utils/models/guild.ts
@@ -0,0 +1,13 @@
+import { Schema, model } from "mongoose";
+import { IGuild } from "../../interface/Guild";
+
+const userSchema = new Schema<IGuild>({
+    id: { type: String, required: true },
+    name: { type: String, required: true },
+    //@ts-ignore
+    data: { type: Array<Object>, required: true },
+    owner: { type: String, required: true },
+    invite: { type: String, required: true },
+})
+
+export const Guild = model<IGuild>('Guilds', userSchema);
