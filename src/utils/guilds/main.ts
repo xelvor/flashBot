ac0c56a34ac37af142472ef431a95d5117bf97c8
Daniel <imoleczek@gmail.com>
2023-08-14 19:36:11 +0200

    Update main.ts

--- src/utils/guilds/main.ts
+++ src/utils/guilds/main.ts
@@ -1,5 +1,18 @@
 import { Client } from "discord.js";
+import { Guild } from "../models/guild";
 
 export async function loadInvites(client: Client, guild: string) {
     
+}
+
+export function newGuild(id: string, name: string, data: Array<object>, owner: string, invite: string) {
+    const guild = new Guild({
+        name: name,
+        id: id,
+        data: data,
+        owner: owner,
+        invite: invite
+    })
+
+    guild.save()
 }
