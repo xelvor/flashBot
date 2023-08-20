aa30d06adeb10fe0dcccfe4c8f4686dd74fb6e00
Daniel <imoleczek@gmail.com>
2023-08-20 20:09:12 +0200

    Update normalCommands.ts

--- src/events/normalCommands.ts
+++ src/events/normalCommands.ts
@@ -1,6 +1,7 @@
-import { Message } from 'discord.js';
+import { EmbedBuilder, Message } from 'discord.js';
 import Event from '../base/Event';
 import { config } from '../config';
+import { nCommands } from '..';
 
 export default class musicButtons extends Event {
     constructor() {
@@ -14,7 +15,20 @@ export default class musicButtons extends Event {
                 const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
                 const command = args.shift().toLowerCase();
 
-                
+                const cmd = nCommands.find(x => x.name === command)
+                if (!cmd) {
+                    const embed = new EmbedBuilder()
+                    .setTitle('<a:nie:1043874712155070504> Error')
+                    .setColor('Red')
+                    .setDescription('This command doesn\'t exist')
+                    .setTimestamp()
+                    .setFooter({
+                        text: message.member.user.username,
+                        iconURL: message.member.user.avatarURL()
+                    })
+                    return await message.reply({ embeds: [embed] })
+                }
+                cmd.run(message, args);
             }
         })
     }
