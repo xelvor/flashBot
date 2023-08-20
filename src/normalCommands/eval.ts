82cf2c6115b23929badcdaa5f41696e8bbd0e4b0
Daniel <imoleczek@gmail.com>
2023-08-20 20:09:20 +0200

    Update eval.ts

--- src/normalCommands/eval.ts
+++ src/normalCommands/eval.ts
@@ -1,4 +1,4 @@
-import { Message } from "discord.js";
+import { EmbedBuilder, HexColorString, Message } from "discord.js";
 import NormalCommand from "../base/NCommand";
 
 export default class evalCommand extends NormalCommand {
@@ -6,8 +6,34 @@ export default class evalCommand extends NormalCommand {
         super({
             name: "eval",
             description: "Evaluates code",
-            run: async (message: Message) => {
-                message.reply('xdd')
+            run: async (message: Message, args: string[]) => {
+                if (!message.member.roles.cache.get('1122948803608199239')) return;
+
+                const code = args.join(" ");
+                try {
+                    const result = await eval(code);
+                    const embed = new EmbedBuilder()
+                    .setTitle('<a:tak:1043874690634096681> Success')
+                    .setDescription(`\`\`\`js\n${result}\`\`\``)
+                    .setColor('Green')
+                    .setTimestamp()
+                    .setFooter({
+                        text: message.member.user.username,
+                        iconURL: message.member.user.avatarURL()
+                    })
+                    await message.reply({ embeds: [embed] });
+                } catch (error) {
+                    const embed = new EmbedBuilder()
+                    .setTitle('<a:nie:1043874712155070504> Error')
+                    .setDescription(`\`\`\`js\n${error}\`\`\``)
+                    .setColor('Red')
+                    .setTimestamp()
+                    .setFooter({
+                        text: message.member.user.username,
+                        iconURL: message.member.user.avatarURL()
+                    })
+                    await message.reply({ embeds: [embed] });
+                }
             },
         });
     }
