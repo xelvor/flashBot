77751ecca680b4cb50aadf273716a07390b559ca
chwytox <141938980+chwytox@users.noreply.github.com>
2023-08-21 00:39:50 +0200

    voiceStateUpdate

--- src/events/voiceStateUpdate.ts
+++ src/events/voiceStateUpdate.ts
@@ -1,4 +1,4 @@
-import { ChannelType, Guild, GuildChannel, GuildMember, VoiceBasedChannel } from 'discord.js';
+import { ChannelType } from 'discord.js';
 import Event from '../base/Event';
 import { client } from '../base/Client';
 
@@ -8,21 +8,43 @@ export default class GuildMemberRemoveEvent extends Event {
             name: 'voiceStateUpdate',
             run: async (oldState, newState) => {
                 const user = newState.member.user;
-                const guild: Guild = newState.guild;
-                const guildMember = guild.members.cache.get(user.id);
+                const guild = newState.guild;
                 const joinedChannel = newState.channel;
                 const leftChannel = oldState.channel;
-                
-                const targetChannelId = '1142938752424099900';
+
+                const targetChannelId = '1142948419413999636';
+                const channelPrefix = 'channel-'
             
                 if (!leftChannel && joinedChannel && joinedChannel.id === targetChannelId) {
-                    const channel: VoiceBasedChannel = await newState.guild.channels.create({
-                        name: `${user.username}`,
+                    let newChannelName = `${channelPrefix}${user.username}`;
+                    
+                    const similarChannels = guild.channels.cache.filter(channel => channel.type === ChannelType.GuildVoice && channel.name.startsWith(channelPrefix));
+
+                    if (similarChannels.some(channel => channel.name === newChannelName)) {
+
+                        let suffixNumber = 1;
+                        while (similarChannels.some(channel => channel.name === `${newChannelName}-${suffixNumber}`)) {
+                            suffixNumber++;
+                        }
+                        newChannelName = `${newChannelName}-${suffixNumber}`;
+
+                    }
+
+                    const channel = await newState.guild.channels.create({
+
+                        name: newChannelName,
                         type: ChannelType.GuildVoice,
                         parent: joinedChannel.parentId,
+
                     });
 
-                    await guildMember.voice.setChannel(channel)
+                    await newState.member.voice.setChannel(channel);
+                }
+
+                if (leftChannel && leftChannel.name.startsWith(channelPrefix) && leftChannel.members.size === 0) {
+
+                    await leftChannel.delete();
+                    
                 }
             },
         });
